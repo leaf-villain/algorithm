060e3b0be4e43d480401b066593051baa0fc4cde
hoodcat6506 <hoodcat6506@gmail.com>
2020-11-18 17:06:55 +0900

    modify format

--- hoodcat6506/leetcode/medium/0005_Longest Palindromic Substring.js
+++ hoodcat6506/leetcode/medium/0005_Longest Palindromic Substring.js
@@ -2,20 +2,20 @@
  * @param {string} s
  * @return {string}
  */
-var longestPalindrome = function(s) {
-    var len = s.length;
-    var dp = Array.from({length: len}, (_, idx) => new Array(len-idx));
+var longestPalindrome = function (s) {
+    var length = s.length;
+    var dp = Array.from({ length }, (_, idx) => new Array(length - idx));
     var result = '';
 
-    for (var i=0; i < dp.length ; i++) {
-        for(var j=0; j < dp[i].length ; j++) {
-            var sub = s.slice(j, j+i+1);
-            if (sub[0] === sub[sub.length-1]) {
+    for (var i = 0; i < dp.length; i++) {
+        for (var j = 0; j < dp[i].length; j++) {
+            var sub = s.slice(j, j + i + 1);
+            if (sub[0] === sub[sub.length - 1]) {
                 var mid = sub.slice(1, -1);
-                if(mid === '' || dp[i-2][j+1]) {
+                if (mid === '' || dp[i - 2][j + 1]) {
                     dp[i][j] = true;
                     result = result.length >= sub.length ? result : sub;
-                }else {
+                } else {
                     dp[i][j] = false;
                 }
             } else {
@@ -23,6 +23,6 @@ var longestPalindrome = function(s) {
             }
         }
     }
-        
+
     return result;
 };
