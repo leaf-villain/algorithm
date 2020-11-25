a9f77bc97350b01705c558b70f573864650620d5
Ch <ccwpc@hanmail.net>
2020-11-25 20:23:25 +0900

    format go code via vscode extension

--- sftblw/leetcode/easy/26_remove-duplicates-from-sorted-array.go
+++ sftblw/leetcode/easy/26_remove-duplicates-from-sorted-array.go
@@ -1,12 +1,15 @@
 // https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 // "생소한 언어"
-package main
 
-import (
-	"fmt"
-)
+// package main
+package problem
 
 // play.golang.go 에서 설치 없이 실행해보려면 아래 메인 함수를 주석 해제합니다.
+// (패키지 이름도 변경해야 함)
+//
+// import (
+// 	"fmt"
+// )
 // func main() {
 //  // array literal
 // 	// https://www.codingnuri.com/golang-book/6.html
@@ -20,13 +23,15 @@ import (
 // }
 
 func removeDuplicates(nums []int) int {
-  // len(arr)
+	// len(arr)
 	// https://tour.golang.org/moretypes/11
-	if len(nums) == 0 { return 0 }
-  
-  // for i, v := range arr {}
-  // https://mingrammer.com/gobyexample/collection-functions/
-  // https://www.joinc.co.kr/w/GoLang/example/range
+	if len(nums) == 0 {
+		return 0
+	}
+
+	// for i, v := range arr {}
+	// https://mingrammer.com/gobyexample/collection-functions/
+	// https://www.joinc.co.kr/w/GoLang/example/range
 	lastIdx := 0
 	lastVal := nums[0]
 	for _, v := range nums {
@@ -35,7 +40,7 @@ func removeDuplicates(nums []int) int {
 			nums[lastIdx] = v
 			lastVal = v
 		}
-  }
-  
+	}
+
 	return lastIdx + 1
 }
