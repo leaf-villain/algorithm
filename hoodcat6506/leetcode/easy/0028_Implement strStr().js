34d12400d9cb919ed8e2f2fe8915808957065c58
hoodcat6506 <hoodcat6506@gmail.com>
2020-12-04 00:22:45 +0900

    Solve problem 28

---
+++ hoodcat6506/leetcode/easy/0028_Implement strStr().js
@@ -0,0 +1,11 @@
+/**
+ * 28. Implement strStr()
+ * https://leetcode.com/problems/implement-strstr
+ *
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+const strStr = function(haystack, needle) {
+  return haystack.indexOf(needle);
+};
